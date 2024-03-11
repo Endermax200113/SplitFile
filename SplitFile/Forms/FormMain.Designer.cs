4b0dac131ddb6e68eaa9ef9660144b702745ae7e
Maxim Hallasaar <endermax200113@mail.ru>
2024-03-12 00:46:09 +0300

    Preparated files

--- SplitFile/Forms/FormMain.Designer.cs
+++ SplitFile/Forms/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SplitFile
 {
-	partial class Form1
+	partial class FormMain
 	{
 		/// <summary>
 		/// Обязательная переменная конструктора.
@@ -28,10 +28,16 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.SuspendLayout();
+			// 
+			// FormMain
+			// 
+			resources.ApplyResources(this, "$this");
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Name = "FormMain";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
---
+++ SplitFile/Forms/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace SplitFile
+{
+	public partial class FormMain : MaterialForm
+	{
+		public FormMain()
+		{
+			InitializeComponent();
+
+			MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+			materialSkinManager.AddFormToManage(this);
+			materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+			materialSkinManager.ColorScheme = new ColorScheme(
+					Primary.BlueGrey800, 
+					Primary.BlueGrey900, 
+					Primary.BlueGrey500, 
+					Accent.LightBlue200, 
+					TextShade.WHITE
+			);
+
+		}
+	}
+}
